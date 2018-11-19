d0992cf6790f66f89c74463fd8bad44fe05c73bc
Marku <markuspotsdam@gmail.com>
2018-11-19 06:53:17 +0000

    november

--- js/jsl.js
+++ js/jsl.js
@@ -3,26 +3,49 @@
  */
 
 function initialiseView() {
-    var header = document.getElementsByClassName("button tile")[0];
-    var body = document.body;
-    var refresh = document.querySelector(".refresh");
-    var main = document.querySelector("main");
+    const header = document.getElementsByClassName("button tile")[0];
+    const body = document.body;
+    const refresh = document.querySelector(".refresh");
+    const main = document.querySelector("main");
+    const add = document.querySelector("button new-item");
+
 
 
     header.onclick = () => {
-        body.classList.toggle("tiles")
+        main.classList.toggle("faded");
+        main.addEventListener("transitionend",onfadeout);
     }
 
     refresh.onclick = () => {
         main.classList.toggle("faded");
-        main.addEventListener("transitioned", onfadeout);
-        body.classList.toggle("tiles");
+        main.addEventListener("transitionend",onfadeout);
+        //body.classList.toggle("tiles");
+
     }
+
+    add.onclick = (evt) => {
+        evt.stopPropagation();
+        var obj = {name: "direm", src: "img/300:100.jpg"};
+        addNewLi(obj,ul);
+    }
+
     prepareListitemSelection();
 
+    xhr("GET","data/listitems.json",null,(xhrobj) => {
+       var listitems = JSON.parse(xhrobj.responseText);
+       cosnole.log("get objects: ", listitems);
+       listitems.forEach(obj => {
+          addNewLi(obj,ul);
+       });
+    });
+
     function onfadeout() {
-        main.removeEventListener("transitioned",onfadeout);
-        main.classList.toogle("fadeout");
+        main.removeEventListener("transitionend",onfadeout);
+        setTimeout(() =>
+        {
+            main.classList.toggle("faded");
+        }, 2500);
+        body.classList.toggle("tiles");
     }
 
     function getListItemName(li) {
@@ -30,15 +53,48 @@ function initialiseView() {
     }
 
     function prepareListitemSelection() {
-        var listitems = document.getElementsByTagName("li");
-        var currentli = null;
-        for (var i=0;i<listitems.length;i++) {
-            currentli = listitems[i];
-            currentli.onclick = () => {
-                alert("selected: " + getListItemName(currentli));
+        // var listitems = document.getElementsByTagName("li");
+        // var currentli = null;
+        // for (var i=0;i<listitems.length;i++) {
+        //     currentli = listitems[i];
+        //     currentli.onclick = () => {
+        //        alert("selected: " + getListItemName(currentli));
+        //    }
+        // }
+        // alert("currentli: " + currentli);
+
+        const listitems = document.querySelector("main li");
+        listitems.forEach(currentli => {
+            currentli.onclick = (evt) => {
+                alert("got event" + evt + "selected: " + getListItemName(currentli))
             }
+        });
+    }
+
+    function addNewLi(obj, listroot) {
+        alert("addNewLI(): " + JSON.stringify(obj));
+        var newli = document.createElement("li");
+        var img = document.createElement("img");
+        img.src = obj.src;
+        img.classList.add("align-left");
+        newli.appendChild(img);
+
+        var name = document.createTextNode(obj.name);
+        newli.appendChild(name);
+
+        listroot.appendChild(newli);
+
+    }
+
+    function lookupLi(el) {
+        if(el.tagName == "LI") {
+            return el;
         }
-        alert("currentli: " + currentli);
+        else if (el.tagName == "BODY") {
+            alert("Reached root of Tree. No LI found");
+            return null;
+        }
+
     }
 }
 
